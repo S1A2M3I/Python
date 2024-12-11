82db96eef492ea7bc93d2bf3ab8115471da92798
S1A2M3I <108212900+S1A2M3I@users.noreply.github.com>
2024-12-11 13:46:29 +0530

    first commit

---
+++ Fi.py
@@ -0,0 +1,105 @@
+#Python is a programming language that lets you work quickly and integrate systems more effectively.
+#It's a general-purpose language that lets you work with data, whether in the cloud or on-premises.
+#Framework is a collection of tools and libraries that can be used to build applications.
+#Framework for Python is the tkinter library which is used to build GUI applications.
+#Python is a well intentended and standard language.
+#Python is a high-level, interpreted, general-purpose programming language.
+#Python is a high-level programming language that is interpreted, which means that the code is executed directly by the   
+#interpreter.
+#Inventory Managment System using python, tkinter library, and MySQL Database.
+#in-built functions in python like print(), input(), etc.
+#Opps and Procedural Programming Language difference between them.
+#Procedural programming is a programming paradigm that uses a collection of functions to build a program.
+#Opps programming is a programming paradigm that uses objects and their interactions to build a program.
+#numpy and pandas are two popular libraries in Python for data analysis and manipulation.
+#Pandas is a popular library for data manipulation and analysis in Python.
+#Numpy is a popular library for numerical computing in Python.
+
+
+print("Hello World")
+print(True)
+#ZeroDivisionError is a arithmetic exception which do not allow illegal mathematical 
+#operations
+
+# print(5/0)
+#datatypes
+#int
+#float
+#list 
+#dictionary
+#tuple
+#set 
+#string 
+#boolean
+
+#type of operators
+#arithmetic operators
+#comparison operators
+#logical operators
+#bitwise operators
+#membership operators
+#identity operators
+#boolean operators
+
+#arithmetic operators
+#addition
+#subtraction
+#multiplication
+#division
+#modulus
+#floor division
+#exponent
+
+num = 5
+num1 = 4
+print(num+num1)
+print(num-num1)
+print(num*num1)
+print(num/num1)
+print(num%num1)
+print(num//num1)
+print(num**num1)
+
+#comparison operators
+#equal to
+#not equal to
+#greater than
+#less than  
+#greater than or equal to
+#less than or equal to
+print(num==num1)
+print(num!=num1)
+print(num>num1)
+print(num<num1)
+print(num>=num1)
+print(num<=num1)
+
+#logical operators
+#and
+#or
+#not
+print(num>num1 and num>num1)
+print(num>num1 or num>num1)
+print(not(num>num1 or num>num1))
+
+#bitwise operators
+#and
+#or
+#not
+#xor
+#left shift
+#right shift
+print(num&num1)
+print(num|num1)
+print(~num)
+print(num^num1)
+print(num<<num1)
+print(num>>num1)
+
+name = input("Enter your name: ")
+print("Name is :",name)
+
+#Type Casting
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+print("Number is: ",num1+num2)
